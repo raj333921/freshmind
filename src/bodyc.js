27efa1bc9c4a7b21885b3436fa22f4bd8b3f4de3
vrkgandla <vrkgandla@gmail.com>
2023-08-25 00:06:43 +0200

    Added deals category

--- src/bodyc.js
+++ src/bodyc.js
@@ -5,7 +5,7 @@ import _ from 'underscore'
 const Bodyc = ({data,query,event}) => {
 const accordian = _.sortBy(data, 'id').map(link => <CustomizedAccordians key={link.id} id={link.id} indexList={link.indexList} date={link.date} author={link.author} category={link.category} />);
 const accordian_query = query.map(link => <CustomizedAccordians key={link.id} id={link.question} answer={link.answer} date={link.date} author={link.author} category={link.category} />);
-const eventTimeLine = event.map(link => <TimeLinec event={link}/>);
+const eventTimeLine = _.sortBy(event, 'id').reverse().map(link => <TimeLinec event={link}/>);
 return(
 <div>
 <Tabsc accordian={accordian} search={data} accordian_query={accordian_query} searchQuery={query} event={eventTimeLine}/>
--- src/components/responsiveAppBar.js
+++ src/components/responsiveAppBar.js
@@ -2,16 +2,34 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import ContactEmergencyTwoToneIcon from '@mui/icons-material/ContactEmergencyTwoTone';
+import _ from 'underscore';
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = [];
+const settings = [{key:'Ambulance', value: '112', valueTel: 'tel:112'},{key:'Police', value: '101',valueTel:'tel:101'},{key:'Medical Service', value: '100',valueTel:'tel:100'},{key:'Fire', value: '100',valueTel:'tel:100'},{key:'Pharmacy', value: 'Website',valueTel:'https://www.pharmacie.be/'}];
 
 function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
@@ -40,15 +58,12 @@ function ResponsiveAppBar() {
             Fresh Mind 1.0
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-
-          </Box>
           <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
             component="a"
-            href=""
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
@@ -60,13 +75,26 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            Fresh Mind 1.0
+            Fresh Mind
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                      </Box>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-
+            <Tooltip title="Open Quick Contacts">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 1 }}>
+              <ContactEmergencyTwoToneIcon size="large" sx={{ fontSize: 40,color: "Red" }}/>
+              </IconButton>
+            </Tooltip>
             <Menu
               sx={{ mt: '45px' }}
               id="menu-appbar"
@@ -83,9 +111,10 @@ function ResponsiveAppBar() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
+              {_.sortBy(settings, 'key').map((setting) => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+                  <Typography textAlign="center">{setting.key} :</Typography>
+                  <a href={setting.valueTel}>{setting.value}</a>
                 </MenuItem>
               ))}
             </Menu>
