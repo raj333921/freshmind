979090767444e7f18febefdc21fd7c2591a69d9f
vrkgandla <vrkgandla@gmail.com>
2023-09-05 20:58:46 +0200

    Fixed free solo

--- src/components/freeSoloQuery.js
+++ src/components/freeSoloQuery.js
@@ -5,16 +5,16 @@ import Autocomplete from '@mui/material/Autocomplete';
 import _ from 'underscore';
 
 export default function FreeSoloQuery({search}) {
-let searchOption = _.flatten(search.map((option) => option),'question');
+let searchOption = _.flatten(search.map((option) => option),'faqQestion');
   return (
     <Stack direction={'column-reverse'}>
       <Autocomplete
         id="free-solo-demo"
         search
-        getOptionLabel ={(option) => option.question}
+        getOptionLabel ={(option) => option.faqQuestion}
         options={searchOption}
         onChange={(event: any, option: any) => {
-            return option? alert(option.answer):'';
+            return option? alert(option.faqAnswer):'';
         }}
         renderInput={(params) => <TextField {...params} label="Each and Every mind have different questions. We captured some of it !!" />}
       />
